ce36e99e37a52a885b05b3790b05792b121147cf
wly185 <wongluyi@gmail.com>
2021-03-19 18:07:16 +0800

    remove dotenv config

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+// const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const connectDB = async () => {
   try {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const handlebars = require('express-handlebars');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+// require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 //mongodb
 connectDB();
